3f0a7195b26a9033f735d932b77dba800584696c
yahyaelsf <yahyaelsaftawi98@gmail.com>
2022-10-04 23:04:06 +0300

    second commit

---
+++ test-js/js/script.js
@@ -0,0 +1,7 @@
+// prompt("Enter passcode");
+// let name = "yahya";
+// console.log(name) ;
+// console.log(Math.max(2, 4));
+// let theNumber = prompt("Pick a number");
+// console.log("Your number is the square root of " +
+//     theNumber * theNumber);
