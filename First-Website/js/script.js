c4fe33c7a1d51a4cbd1a4f965f9ceda6a7430465
yahyaelsf <yahyaelsaftawi98@gmail.com>
2022-10-14 23:34:23 +0300

    First Website

---
+++ First-Website/js/script.js
@@ -0,0 +1,111 @@
+// ************************************ [1] Transparent Header on first section after that header will has background
+window.addEventListener('scroll', () => {
+    let scrollValue = window.scrollY;
+    if (scrollValue > 122) {
+        document.querySelector('header').classList.add('bg');
+    } else {
+        document.querySelector('header').classList.remove('bg');
+    }
+})
+
+// ************************************ [2] Smooth scroll to section
+document.querySelectorAll('a[href^="#"]').forEach((el) => {
+    el.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector(el.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+});
+
+// ************************************ [3] Our Work Filter
+let front = document.querySelector('#front'),
+    back = document.querySelector('#back'),
+    seo = document.querySelector('#seo'),
+    hosting = document.querySelector('#hosting'),
+    Sfront = document.querySelector('.front'),
+    Sback = document.querySelector('.back'),
+    Sseo = document.querySelector('.seo'),
+    Shosting = document.querySelector('.hosting');
+
+front.onclick = () => {
+    Sfront.classList.remove('hide');
+    front.classList.add('active');
+    Sback.classList.add('hide');
+    back.classList.remove('active');
+    Sseo.classList.add('hide');
+    seo.classList.remove('active');
+    Shosting.classList.add('hide');
+    hosting.classList.remove('active');
+}
+back.onclick = () => {
+    Sfront.classList.add('hide');
+    front.classList.remove('active');
+    Sback.classList.remove('hide');
+    back.classList.add('active');
+    Sseo.classList.add('hide');
+    seo.classList.remove('active');
+    Shosting.classList.add('hide');
+    hosting.classList.remove('active');
+}
+seo.onclick = () => {
+    Sfront.classList.add('hide');
+    front.classList.remove('active');
+    Sback.classList.add('hide');
+    back.classList.remove('active');
+    Sseo.classList.remove('hide');
+    seo.classList.add('active');
+    Shosting.classList.add('hide');
+    hosting.classList.remove('active');
+}
+hosting.onclick = () => {
+    Sfront.classList.add('hide');
+    front.classList.remove('active');
+    Sback.classList.add('hide');
+    back.classList.remove('active');
+    Sseo.classList.add('hide');
+    seo.classList.remove('active');
+    Shosting.classList.remove('hide');
+    hosting.classList.add('active');
+}
+
+// ************************************ [4] Scroll Top
+
+
+
+// ************************************ [5] Form Validation with error messages 
+
+let btn = document.querySelector('#btn'),
+    inputname = document.querySelector('#name').value,
+    inputemail = document.querySelector('#email').value,
+    inputcontact = document.querySelector('#contact').value;
+btn.onclick = () => {
+    if (inputname === '') {
+        document.querySelector('.name').classList.remove('hide');
+    } if (inputemail === '') {
+        document.querySelector('.email').classList.remove('hide');
+    }
+    // if (inputcontact === '') {
+    //     document.querySelector('.contact').classList.remove('hide');
+    // }
+}
+// ************************************ [7] Loading screen with percentage of loading
+let progres = document.querySelector('.progres'),
+    section = document.querySelector('section');
+
+let number = document.querySelector('.number');
+let counter = 0;
+setInterval(() => {
+    if (counter == 100) {
+        progres.setAttribute('class','hidden');
+        section.classList.remove('hidden');
+        clearInterval();
+
+
+    } else {
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+
+}, 20);
+
--- test-js/js/script.js
+++ test-js/js/script.js
@@ -4,4 +4,5 @@
 // console.log(Math.max(2, 4));
 // let theNumber = prompt("Pick a number");
 // console.log("Your number is the square root of " +
-//     theNumber * theNumber);
+//     theNumber * theNumber)
+console.log(Math.floor(Math.random()*30)+20)
